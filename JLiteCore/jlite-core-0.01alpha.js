1094be0379a3f2b050bf9cf87858601e6238d159
Yan Qizheng <yanqizheng@live.cn>
2012-11-30 23:21:02 +0800

    add jlite-core develop file

---
+++ JLiteCore/jlite-core-0.01alpha.js
@@ -0,0 +1,13 @@
+/*
+ * jLite:Be Lite,Do More and Fast!
+ * verion:0.01 alpha
+ * author:yanqizheng
+ */
+
+var jlite=function(selector){
+    var id=selector;
+    var elem=window.document.getElementById(id);
+    return elem;
+}
+
+var $=jlite;
